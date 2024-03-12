8ef8f90674141b77479e16ae45243e712d4f1d70
Manu7K <emmanuel.kouame@std.heh.be>
2024-03-12 16:15:23 +0100

    ajout des data

---
+++ src/db/mockPokemon.js
@@ -0,0 +1,143 @@
+module.exports  = [
+    {
+        name: "Bulbizarre",
+        id: 1,
+        hp: 25,
+        types: ["Plante", "Poisson"],
+        created: "2024-02-27T12:00:00.000Z"
+    },
+    {
+        name: "Salameche",
+        id: 2,
+        hp: 30,
+        types: ["Feu"],
+        created: "2024-02-27T12:10:00.000Z"
+    },
+    {
+        name: "Carapuce",
+        id: 3,
+        hp: 28,
+        types: ["Eau"],
+        created: "2024-02-27T12:20:00.000Z"
+    },
+    {
+        name: "Pikachu",
+        id: 4,
+        hp: 35,
+        types: ["Electrik"],
+        created: "2024-02-27T12:30:00.000Z"
+    },
+    {
+        name: "Roucool",
+        id: 5,
+        hp: 20,
+        types: ["Vol", "Normal"],
+        created: "2024-02-27T12:40:00.000Z"
+    },
+    {
+        name: "Rattata",
+        id: 6,
+        hp: 22,
+        types: ["Normal"],
+        created: "2024-02-27T12:50:00.000Z"
+    },
+    {
+        name: "Aspicot",
+        id: 7,
+        hp: 24,
+        types: ["Insecte", "Poison"],
+        created: "2024-02-27T13:00:00.000Z"
+    },
+    {
+        name: "Nosferapti",
+        id: 8,
+        hp: 27,
+        types: ["Vol", "Poison"],
+        created: "2024-02-27T13:10:00.000Z"
+    },
+    {
+        name: "Mystherbe",
+        id: 9,
+        hp: 26,
+        types: ["Plante", "Poison"],
+        created: "2024-02-27T13:20:00.000Z"
+    },
+    {
+        name: "Taupiqueur",
+        id: 10,
+        hp: 29,
+        types: ["Sol"],
+        created: "2024-02-27T13:30:00.000Z"
+    },
+    {
+        name: "Miaouss",
+        id: 11,
+        hp: 31,
+        types: ["Normal"],
+        created: "2024-02-27T13:40:00.000Z"
+    },
+    {
+        name: "Psykokwak",
+        id: 12,
+        hp: 32,
+        types: ["Eau"],
+        created: "2024-02-27T13:50:00.000Z"
+    },
+    {
+        name: "Ferosinge",
+        id: 13,
+        hp: 33,
+        types: ["Combat"],
+        created: "2024-02-27T14:00:00.000Z"
+    },
+    {
+        name: "Caninos",
+        id: 14,
+        hp: 34,
+        types: ["Feu"],
+        created: "2024-02-27T14:10:00.000Z"
+    },
+    {
+        name: "Ptitard",
+        id: 15,
+        hp: 25,
+        types: ["Eau"],
+        created: "2024-02-27T14:20:00.000Z"
+    },
+    {
+        name: "Abra",
+        id: 16,
+        hp: 26,
+        types: ["Psy"],
+        created: "2024-02-27T14:30:00.000Z"
+    },
+    {
+        name: "Machoc",
+        id: 17,
+        hp: 30,
+        types: ["Combat"],
+        created: "2024-02-27T14:40:00.000Z"
+    },
+    {
+        name: "Ponyta",
+        id: 18,
+        hp: 32,
+        types: ["Feu"],
+        created: "2024-02-27T14:50:00.000Z"
+    },
+    {
+        name: "Tentacool",
+        id: 19,
+        hp: 28,
+        types: ["Eau", "Poison"],
+        created: "2024-02-27T15:00:00.000Z"
+    },
+    {
+        name: "Spectrum",
+        id: 20,
+        hp: 27,
+        types: ["Spectre"],
+        created: "2024-02-27T15:10:00.000Z"
+    }
+
+]
